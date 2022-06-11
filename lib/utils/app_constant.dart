0760866d9453b9a304d181ad08e9e12053c2d103
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2022-06-11 12:08:21 +0700

    add: home screen with google maps

--- lib/utils/app_constant.dart
+++ lib/utils/app_constant.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:metro_tracking_new/screens/home_screen.dart';
 import 'package:metro_tracking_new/screens/login_screen.dart';
 
 class AppConstant {
   static Map<String, Widget Function(BuildContext)> route = {
     '/login': (context) => const LoginScreen(),
+    '/home' : (context) => const HomeScreen(),
   };
 }
 
