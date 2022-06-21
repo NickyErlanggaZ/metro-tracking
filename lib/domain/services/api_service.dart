9f164f64ac3c8969933c6a70c53ef9651011bfbd
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2022-06-21 15:34:38 +0700

    delete: unused function

--- lib/domain/services/api_service.dart
+++ lib/domain/services/api_service.dart
@@ -5,23 +5,5 @@ import 'package:metro_tracking_new/config/api_config.dart';
 class ApiService {
   final String url = ApiConfig.baseUrl;
   final client = http.Client();
-
-  postData(data, apiURL) async {
-    var fullUrl = url + apiURL;
-    return await client.post(Uri.parse(fullUrl),
-        body: json.decode(data), headers: _setHeaders());
-  }
   
-  getData(apiURL) async {
-    var fullUrl = url + apiURL;
-    return await client.get(
-      Uri.parse(fullUrl),
-      headers: _setHeaders(),
-    );
-  }
-
-  _setHeaders() => {
-        'Content-type': 'application/json',
-        'Accept': '*/*',
-      };
 }
