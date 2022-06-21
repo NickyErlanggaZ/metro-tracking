434ba85729d645d5c037b172f06fe5444fff4a06
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2022-06-21 15:33:35 +0700

    update: wrap main app with oktoast

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,16 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:oktoast/oktoast.dart';
 import 'package:metro_tracking_new/screens/splash_screen.dart';
 import 'package:metro_tracking_new/utils/app_constant.dart';
 import 'package:metro_tracking_new/utils/color_constant.dart';
 
 void main() {
   runApp(const MyApp());
-   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
- ));
+  ));
   HttpOverrides.global = MyHttpOverrides();
 }
 
@@ -19,15 +20,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Metro Tracking',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primaryColor: ColorConstant.primaryColor,
-          backgroundColor: ColorConstant.backgroundColor,
-          fontFamily: 'Poppins'),
-      routes: AppConstant.route,
-      home: const SplashScreen(),
+    return OKToast(
+      position: ToastPosition.bottom,
+      child: MaterialApp(
+        title: 'Metro Tracking',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primaryColor: ColorConstant.primaryColor,
+            backgroundColor: ColorConstant.backgroundColor,
+            outlinedButtonTheme: OutlinedButtonThemeData(
+              style: OutlinedButton.styleFrom(
+                primary: ColorConstant.inActiveColor,
+                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+                shape: const StadiumBorder(),
+                side: BorderSide(color: ColorConstant.inActiveColor,)
+              ),
+            ),
+            fontFamily: 'Poppins'),
+        routes: AppConstant.route,
+        home: const SplashScreen(),
+      ),
     );
   }
 }
