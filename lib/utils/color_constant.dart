4cfe4eab16f1dd1354000c0f0d305ba0c4cb0333
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2022-06-21 15:36:29 +0700

    update: change inactive color code

--- lib/utils/color_constant.dart
+++ lib/utils/color_constant.dart
@@ -4,5 +4,5 @@ class ColorConstant{
   static Color primaryColor = const Color(0xFFF3C201);
   static Color secondaryColor = const Color(0xFF292929);
   static Color backgroundColor = const Color(0xFFFEFEFE);
-  static Color inActiveColor = const Color(0xFF555555);
+  static Color inActiveColor = const Color(0xFF878787);
 }
