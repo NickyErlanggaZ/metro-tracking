16f169f7a6065f99050b601c7fce1a7190e92d5b
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2022-06-27 03:44:10 +0700

    add: yellow shade color

--- lib/utils/color_constant.dart
+++ lib/utils/color_constant.dart
@@ -5,4 +5,5 @@ class ColorConstant{
   static Color secondaryColor = const Color(0xFF292929);
   static Color backgroundColor = const Color(0xFFFEFEFE);
   static Color inActiveColor = const Color(0xFF878787);
+  static Color yellowShade = const Color(0x63F8DB79);
 }
