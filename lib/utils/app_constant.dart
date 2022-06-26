77b8db149e764ea4a9c69c3b5dff7db0c31fa94f
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2022-06-27 03:43:31 +0700

    add: change password route and delete profile and track route

--- lib/utils/app_constant.dart
+++ lib/utils/app_constant.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:metro_tracking_new/screens/change_password_screen.dart';
 import 'package:metro_tracking_new/screens/home_screen.dart';
 import 'package:metro_tracking_new/screens/login_screen.dart';
-import 'package:metro_tracking_new/screens/profile_screen.dart';
-import 'package:metro_tracking_new/screens/track_screen.dart';
 
 class AppConstant {
   static Map<String, Widget Function(BuildContext)> route = {
     '/login': (context) => const LoginScreen(),
     '/home' : (context) => const HomeScreen(),
-    '/profile' : (context) => ProfileScreen(),
-    '/track' : (context) => const TrackScreen(),
+    '/change-password' : (context) => const ChangePasswordScreen(),
   };
 }
 
