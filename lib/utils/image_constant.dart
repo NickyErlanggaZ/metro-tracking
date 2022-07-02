cabdc12939212a25c7abea9a56d0d1b73e1bfd5a
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2022-07-02 12:00:27 +0700

    add: url asset for marker and change url asset logo

--- lib/utils/image_constant.dart
+++ lib/utils/image_constant.dart
@@ -1,3 +1,6 @@
 class ImageConstant{
-  static String logo = "assets/img/ic_metromesin.png";
+  static String logo = "assets/img/ic_metrotracking.png";
+  static String truk = "assets/img/truck.png";
+  static String mobil = "assets/img/car.png";
+  static String motor = "assets/img/motorbike.png";
 }
