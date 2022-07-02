86b3bcb4a468ddae03b94ce3b08a6ce6dbd7b86d
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2022-07-02 12:00:51 +0700

    add: dark color

--- lib/utils/color_constant.dart
+++ lib/utils/color_constant.dart
@@ -5,5 +5,6 @@ class ColorConstant{
   static Color secondaryColor = const Color(0xFF292929);
   static Color backgroundColor = const Color(0xFFFEFEFE);
   static Color inActiveColor = const Color(0xFF878787);
+  static Color dark = const Color(0xFF555555);
   static Color yellowShade = const Color(0x63F8DB79);
 }
